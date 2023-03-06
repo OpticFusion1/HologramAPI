633a2f4b8af99d14cf8e279e58b0c08ef4f0e368
OpticFusion1 <37254722+OpticFusion1@users.noreply.github.com>
2023-03-06 15:13:18 -0500

    Version 0.1.0 Plugin now loads

---
+++ src/main/java/optic_fusion1/hologram/HologramAPI.java
@@ -0,0 +1,11 @@
+package optic_fusion1.hologram;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HologramAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+}
